b8e51cf905d8dae077a0d1f7d2c2ebc9721b7a0f
master3668 <73420603+master3668@users.noreply.github.com>
2021-01-02 14:40:36 +0800

    Update jd_dreamFactory.js

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -16,7 +16,7 @@ const JD_API_HOST = 'https://m.jingxi.com';
 const notify = $.isNode() ? require('./sendNotify') : '';
 let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
 const randomCount = $.isNode() ? 20 : 5;
-const tuanActiveId = `oJr5P1FhOCf_mB-zN2nPFQ==`;
+const tuanActiveId = `6S9y4sJUfA2vPQP6TLdVIQ==`;
 const jxOpenUrl = `openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%20%22des%22:%20%22m%22,%20%22url%22:%20%22https://wqsd.jd.com/pingou/dream_factory/index.html%22%20%7D`;
 let cookiesArr = [], cookie = '', message = '';
 const inviteCodes = ['oJr5P1FhOCf_mB-zN2nPFQ==', 'gfPtYFVYZOCfTso5bGaFlg==', '8Q1XpCcDmDS_zwr8QKH2eA==', 'iH9A-bqytATkuvaAG2-mHg==', 'hIJbUdlI1dBaOUYNPv3QHw==', 'oSXkG0Q_u_MyB8EErsy_WA==';
